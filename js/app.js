294f1cf5a9f7f05639cbeea1eb1aac1d25821f0c
KhantThuHtet <khantthu7007@gmail.com>
2021-12-06 19:16:19 +0630

    ui done

---
+++ js/app.js
@@ -0,0 +1,23 @@
+let url2 = `https://api.covid19api.com/summary`;
+let url3 = `https://jsonplaceholder.typicode.com/todos`;
+
+// let fetchData = async () =>{
+//   let response = 
+// }
+
+let a = 10;
+let b = 20;
+
+const demo = ()=>{
+  console.log('ae')
+  console.log('be')
+  if (a < b) {
+    throw new Error('Have a nice day');
+  }
+  console.log('de')
+}
+
+console.log(demo());
+
+
+console.log('Ending')
